a965ea86101db2814c1178455e7cb4a2ac7c1a95
jrugis <j.rugis@auckland.ac.nz>
2023-06-27 16:44:28 +1200

    title

--- html_generation/make_html.py
+++ html_generation/make_html.py
@@ -27,7 +27,7 @@ def write_head(f):
 # FUNCTION: 
 def write_title(f):
     f.write('<p>  </p><br>\n')
-    f.write('<big><big>The Developmental Morphology of<br>\n')
+    f.write('<big><big>The Development Morphology of<br>\n')
     f.write('New Zealand Native Orchids</big></big><br>\n')
     f.write('<small>by John Rugis</small>\n')
 
